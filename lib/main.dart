53800c745ebe48bb2bfd649af1943d092e7ac479
Bobby5390 <Salakin15-5390@diu.edu.bd>
2025-02-18 11:48:07 +0600

    salakin 1

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -11,26 +12,38 @@ class LabClass05 extends StatelessWidget {
     return MaterialApp(
       title: "Basic Flutter UI - 02",
       home: Scaffold(
-        appBar: AppBar( 
+        appBar: AppBar(
           title: Text("Basic Flutter UI - 02"),
           backgroundColor: Colors.blue,
         ),
-        body: SingleChildScrollView(
-          scrollDirection: Axis.horizontal,
-          child: Container(
-            height: 600,
-            child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: List.generate(
-                  100,
-                  (index) => Icon(
-                    Icons.access_alarm_rounded,
-                    size: index.toDouble(),
-                    color: Colors.black,
-                  ),
-                )),
-          ),
+        body: Row(
+          children: [
+            Icon(
+              Icons.star,
+              size: 50,
+              color: const Color.fromARGB(227, 22, 210, 50),
+            ),
+            Icon(
+              Icons.star,
+              size: 50,
+              color: const Color.fromARGB(202, 255, 176, 7),
+            ),
+            Icon(
+              Icons.star,
+              size: 50,
+              color: const Color.fromARGB(202, 7, 218, 255),
+            ),
+            Icon(
+              Icons.star,
+              size: 50,
+              color: const Color.fromARGB(211, 255, 7, 7),
+            ),
+            Icon(
+              Icons.star,
+              size: 50,
+              color: const Color.fromARGB(53, 255, 7, 85),
+            ),
+          ],
         ),
       ),
     );
